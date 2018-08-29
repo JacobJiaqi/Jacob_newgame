0b9583f7efeb83b17b12cb0ce96d9707decb1406
陆家祺 <lujiaqi@dyn-118-139-80-65.its.monash.edu.au>
2018-08-29 21:04:18 +1000

    changes

--- trydjango/posts/classification.py
+++ trydjango/posts/classification.py
@@ -1,206 +1,83 @@
-import matplotlib.pyplot as plt
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
 import tensorflow as tf
 import cv2
 import numpy as np
 import os
 from random import shuffle
 from tqdm import tqdm
-import tflearn
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
-
-class image_identify():
-TRAIN_DIR = './Downloads/all/train'
-TEST_DIR = './Downloads/all/test'
-IMG_SIZE = 50
-LR = 1e-3
-MODEL_NAME = 'plant-{}-{}.model'.format(LR, '2conv-basic')
+import matplotlib.pyplot as plt
 
 
-def label_img(img):
-    word_label = img[:7]
-    if word_label == 'healthy':
-        return [1, 0]
-    elif word_label == 'disease':
-        return [0, 1]
+class Image_identify():
 
-
-def create_train_data():
-    training_data = []
-    for img in tqdm(os.listdir(TRAIN_DIR)):
-        if img[:7] == "healthy" or img[:7] == "disease":
-            label = label_img(img)
-            # print(img)
-            path = os.path.join(TRAIN_DIR, img)
-            img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-            # print(img)
-            img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
-            training_data.append([np.array(img), np.array(label)])
-    shuffle(training_data)
-    np.save('train_data.npy', training_data)
-    return training_data
-
-
-def process_test_data():
-    testing_data = []
-    for img in tqdm(os.listdir(TEST_DIR)):
+    def process_test_data(test_data):
+        IMG_SIZE = 50
+        LR = 1e-3
+        testing_data = []
+        list=[]
+        for img in tqdm(os.listdir(test_data)):
+            list.append(img)
         if img != '.DS_Store':
-            # print(img)
-            path = os.path.join(TEST_DIR, img)
-            img_num = img.split('.')[0]
+            img = list[0]
+            path = os.path.join(test_data, img)
+            img_num = img.split('.')[-1]
             img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-            # print(img)
             img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
             testing_data.append([np.array(img), img_num])
+        shuffle(testing_data)
+        np.save('test_data.npy', testing_data)
+        test_data = testing_data
+        fig = plt.figure()
+        for num, data in enumerate(test_data):
+            img_data = data[0]
 
-    shuffle(testing_data)
-    np.save('test_data.npy', testing_data)
-    return testing_data
+            y = fig.add_subplot(3, 4, num + 1)
+            orig = img_data
+            data = img_data.reshape(IMG_SIZE, IMG_SIZE, 1)
 
+            tf.reset_default_graph()
 
-train_data = create_train_data()
+            convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
 
+            convnet = conv_2d(convnet, 32, 5, activation='relu')
+            convnet = max_pool_2d(convnet, 5)
 
-convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+            convnet = conv_2d(convnet, 64, 5, activation='relu')
+            convnet = max_pool_2d(convnet, 5)
 
-convnet = conv_2d(convnet, 32, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
+            convnet = conv_2d(convnet, 128, 5, activation='relu')
+            convnet = max_pool_2d(convnet, 5)
 
-convnet = conv_2d(convnet, 64, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
+            convnet = conv_2d(convnet, 64, 5, activation='relu')
+            convnet = max_pool_2d(convnet, 5)
 
-convnet = fully_connected(convnet, 1024, activation='relu')
-convnet = dropout(convnet, 0.8)
+            convnet = conv_2d(convnet, 32, 5, activation='relu')
+            convnet = max_pool_2d(convnet, 5)
 
-convnet = fully_connected(convnet, 2, activation='softmax')
-convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+            convnet = fully_connected(convnet, 1024, activation='relu')
+            convnet = dropout(convnet, 0.8)
 
-model = tflearn.DNN(convnet, tensorboard_dir='log')
-if os.path.exists('{}.meta'.format(MODEL_NAME)):
-    model.load(MODEL_NAME)
-    # print('model loaded!')
-train = train_data[:50]
-test = train_data[-50:]
-X = np.array([i[0] for i in train], dtype=np.float64).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-Y = np.array([i[1] for i in train], dtype=np.float64)
+            convnet = fully_connected(convnet, 2, activation='softmax')
+            convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy',
+                                 name='targets')
 
-test_x = np.array([i[0] for i in test], dtype=np.float64).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-test_y = np.array([i[1] for i in test], dtype=np.float64)
-model.fit({'input': X}, {'targets': Y}, n_epoch=3, validation_set=({'input': test_x}, {'targets': test_y}),
-          snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+            model_import = tflearn.DNN(convnet, tensorboard_dir='log')
 
-tf.reset_default_graph()
-convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+            model_import.load("./MODEL_NAME", weights_only=False)
 
-convnet = conv_2d(convnet, 32, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
+            model_out = model_import.predict([data])[0]
 
-convnet = conv_2d(convnet, 64, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = conv_2d(convnet, 128, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = conv_2d(convnet, 64, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = conv_2d(convnet, 32, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = fully_connected(convnet, 1024, activation='relu')
-convnet = dropout(convnet, 0.8)
-
-convnet = fully_connected(convnet, 2, activation='softmax')
-convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
-
-model = tflearn.DNN(convnet, tensorboard_dir='log')
-
-if os.path.exists('{}.meta'.format(MODEL_NAME)):
-    model.load(MODEL_NAME)
-    # print('model loaded!')
-
-train = train_data[:-500]
-test = train_data[-500:]
-
-X = np.array([i[0] for i in train]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-Y = [i[1] for i in train]
-
-test_x = np.array([i[0] for i in test]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-test_y = [i[1] for i in test]
-
-model.fit({'input': X}, {'targets': Y}, n_epoch=3, validation_set=({'input': test_x}, {'targets': test_y}),
-          snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
-
-tf.reset_default_graph()
-convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
-
-convnet = conv_2d(convnet, 32, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = conv_2d(convnet, 64, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = conv_2d(convnet, 128, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = conv_2d(convnet, 64, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = conv_2d(convnet, 32, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = fully_connected(convnet, 1024, activation='relu')
-convnet = dropout(convnet, 0.8)
-
-convnet = fully_connected(convnet, 2, activation='softmax')
-convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
-
-model = tflearn.DNN(convnet, tensorboard_dir='log')
-
-if (os.path.exists('{}.meta'.format(MODEL_NAME))):
-    model.load(MODEL_NAME)
-    # print ('Model loaded')
-# if os.path.exists('C:/Users/H/Desktop/healthyvsdisease/{}.meta'.format(MODEL_NAME)):
-# model.load(MODEL_NAME)
-# print('model loaded!')
-
-train = train_data[:-50]
-test = train_data[-50:]
-
-X = np.array([i[0] for i in train]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-Y = [i[1] for i in train]
-
-test_x = np.array([i[0] for i in test]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-test_y = [i[1] for i in test]
-
-model.fit({'input': X}, {'targets': Y}, n_epoch=10, validation_set=({'input': test_x}, {'targets': test_y}),
-          snapshot_step=50, show_metric=True, run_id=MODEL_NAME)
-
-model.save(MODEL_NAME)
-
-
-test_data = process_test_data()
-
-fig = plt.figure()
-
-for num, data in enumerate(test_data[:12]):
-
-    img_num = data[1]
-    img_data = data[0]
-
-    y = fig.add_subplot(3, 4, num + 1)
-    orig = img_data
-    data = img_data.reshape(IMG_SIZE, IMG_SIZE, 1)
-    model_out = model.predict([data])[0]
-
-    if np.argmax(model_out) == 1:
-        str_label = 'disease'
-    else:
-        str_label = 'healthy'
-
-    y.imshow(orig, cmap='gray')
-    plt.title(str_label)
-    y.axes.get_xaxis().set_visible(False)
-    y.axes.get_yaxis().set_visible(False)
-plt.show()
+            if np.argmax(model_out) == 1:
+                str_label = 'disease'
+            else:
+                str_label = 'healthy'
+            y.imshow(orig, cmap='gray')
+            plt.title(str_label)
+            y.axes.get_xaxis().set_visible(False)
+            y.axes.get_yaxis().set_visible(False)
+        plt.show()
+        plt.show()
+        return str_label
