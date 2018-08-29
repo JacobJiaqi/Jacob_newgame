61e05b39be5fc93623c02625ac08590fad586db9
陆家祺 <lujiaqi@dyn-118-139-80-65.its.monash.edu.au>
2018-08-30 04:50:27 +1000

    aa

--- trydjango/posts/classification.py
+++ trydjango/posts/classification.py
@@ -1,7 +1,7 @@
-import tflearn
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
+# import tflearn
+# from tflearn.layers.conv import conv_2d, max_pool_2d
+# from tflearn.layers.core import input_data, dropout, fully_connected
+# from tflearn.layers.estimator import regression
 import tensorflow as tf
 import cv2
 import numpy as np
